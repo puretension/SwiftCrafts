0e1725352e5460e3ff67977836984abc103c488f
puretension <rlrlfhtm5@gmail.com>
2024-01-14 04:34:17 +0900

    CH2. ListView

---
+++ WarmUp/WarmUp/CH2-Listview/ListLoop.swift
@@ -0,0 +1,18 @@
+//
+//  ListLoop.swift
+//  WarmUp
+//
+//  Created by 이도형 on 1/14/24.
+//
+
+import SwiftUI
+
+struct ListLoop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLoop()
+}
