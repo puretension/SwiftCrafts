db2e45aaf7afc4b52d85093d8d799e88f7e4db42
puretension <rlrlfhtm5@gmail.com>
2024-01-14 17:36:10 +0900

    UIKit learning with Storyboard

---
+++ MyUIKit/MyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUIKit
+//
+//  Created by 이도형 on 1/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
